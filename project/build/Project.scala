f9d83c4f6866f572beeb4207f56ae168d4489a86
Nathan Hamblen <nathan@technically.us>
2011-01-10 22:47:21 -0500

    dispatch and unfiltered versions

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,14 +3,14 @@ import sbt._
 class Project(info: ProjectInfo) extends AppengineProject(info)
   with DataNucleus {
 
-  val uf_version = "0.3.0-SNAPSHOT"
+  val uf_version = "0.3.0"
 
   lazy val uff = "net.databinder" %% "unfiltered-filter" % uf_version
   lazy val ufj = "net.databinder" %% "unfiltered-jetty" % uf_version
   val ufjs = "net.databinder" %% "unfiltered-json" % uf_version
 
   lazy val dispatch_gae = "net.databinder" %% "dispatch-http-gae" % "0.8.0.Beta2"
-  lazy val mu = "net.databinder" %% "dispatch-meetup" % "0.7.8-SNAPSHOT"
+  lazy val mu = "net.databinder" %% "dispatch-meetup" % "0.7.9-SNAPSHOT"
   lazy val dbjs = "net.databinder" %% "dispatch-lift-json" % "0.7.8"
 
   // persistence
